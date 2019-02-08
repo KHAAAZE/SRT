67b0bedec053c8e05c13929fda58b83ca40df1a8
Scott <scottrusselltan@gmail.com>
2019-02-08 15:47:52 +0800

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import firebase from "firebase";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Modal, ModalHeader, ModalBody } from "reactstrap";
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
 firebase.initializeApp({
@@ -31,10 +30,10 @@ class Login extends Component {
 
   state = { isSignedIn: false };
   uiConfig = {
-    signInFlow: "popup",
+    signInFlow: "",
     signInOptions: [
-      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.TwitterAuthProvider.PROVIDER_ID,
       firebase.auth.GithubAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID
@@ -52,60 +51,41 @@ class Login extends Component {
   };
 
   render() {
-    const closeBtn = (
-      <button className="close" onClick={this.toggle}>
-        &times;
-      </button>
-    );
-
     return (
       <div className="App">
         <table className="titleBar">
           <tbody>
             <tr>
               <td>
-                <img alt="sample" width="80" src="green_app_icon.svg" />
+                <img alt="sample" width="80" src="truck-icon-0.png" />
               </td>
               <td width="10" />
               <td>
-                <h1 className="movieDB">MovieDB Search</h1>
+                <h1 className="movieDB">SRT Transport</h1>
               </td>
               <td />
               <td className="buttonGap" />
-              <td>
-                <button className="btn btn-success m-3" onClick={this.toggle}>
-                  Login
-                </button>
-              </td>
               <td />
             </tr>
           </tbody>
         </table>
-        <Modal
-          isOpen={this.state.modal}
-          toggle={this.toggle}
-          className={this.props.className}
-        >
-          <ModalHeader toggle={this.toggle} close={closeBtn}>
-            Login
-          </ModalHeader>
-          <ModalBody className="modalLogin">
-            {this.state.isSignedIn ? (
-              <span>
-                <div>Signed In!</div>
-                <button onClick={() => firebase.auth().signOut()}>
-                  Sign out!
-                </button>
-                <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
-              </span>
-            ) : (
-              <StyledFirebaseAuth
-                uiConfig={this.uiConfig}
-                firebaseAuth={firebase.auth()}
-              />
-            )}
-          </ModalBody>
-        </Modal>
+        {this.state.isSignedIn ? (
+          <span>
+            <div>Signed In!</div>
+            <button
+              className="btn btn-danger"
+              onClick={() => firebase.auth().signOut()}
+            >
+              Sign out!
+            </button>
+            <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+          </span>
+        ) : (
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        )}
       </div>
     );
   }
