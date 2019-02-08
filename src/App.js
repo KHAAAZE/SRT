738e18039905b1870f3178ca71e3fc79a9def331
Scott <scottrusselltan@gmail.com>
2019-02-08 13:18:41 +0800

    install the firebase to webapp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebase/StyleFirebaseAuth";
 
 class App extends Component {
+  state = { isSignedIn: false };
   render() {
-    return App;
+    return this.state.isSignedIn ? (
+      <div> Signed In!</div>
+    ) : (
+      <div> Not Signed In!</div>
+    );
   }
 }
 
